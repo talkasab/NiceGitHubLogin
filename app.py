304d87c075a211aac9ba384c2ddcce530af580de
Tarik Alkasab <tarik@alkasab.org>
2024-11-24 13:44:54 -0500

    Further refactoring of configuration.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from urllib.parse import unquote_plus
 
 import requests
 import uvicorn
+from attr import dataclass
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.requests import Request
@@ -17,13 +18,46 @@ main = FastAPI()
 GITHUB_AUTHORIZE_URL: Final[str] = "https://github.com/login/oauth/authorize"
 GITHUB_ACCESS_TOKEN_URL: Final[str] = "https://github.com/login/oauth/access_token"
 GITHUB_USER_INFO_URL: Final[str] = "https://api.github.com/user"
+LOGIN_PATH: Final[str] = "/login"
+DEFAULT_PORT = 5000
 
-CONFIG = {
-    "CLIENT_ID": os.getenv("CLIENT_ID"),
-    "CLIENT_SECRET": os.getenv("CLIENT_SECRET"),
-    "REDIRECT_URI": os.getenv("REDIRECT_URI"),
-    "STORAGE_SECRET": os.getenv("STORAGE_SECRET"),
-}
+
+@dataclass
+class Config:
+    client_id: str | None
+    client_secret: str | None
+    redirect_uri: str | None
+    storage_secret: str | None
+    enable_ssl: bool = False
+    ssl_certfile: str | None = None
+    ssl_keyfile: str | None = None
+    port: int = DEFAULT_PORT
+
+    @property
+    def fixed_redirect_uri(self) -> str:
+        if not self.redirect_uri:
+            raise ValueError("Redirect URI is not set")
+        if self.enable_ssl:
+            return self.redirect_uri.replace("http", "https")
+        return self.redirect_uri.replace("https", "http")
+
+    @property
+    def redirect_uri_path(self) -> str:
+        if not self.redirect_uri:
+            raise ValueError("Redirect URI is not set")
+        return self.redirect_uri.split("://")[1]
+
+
+config = Config(
+    client_id=os.getenv("CLIENT_ID"),
+    client_secret=os.getenv("CLIENT_SECRET"),
+    redirect_uri=os.getenv("REDIRECT_URI"),
+    storage_secret=os.getenv("STORAGE_SECRET"),
+    enable_ssl=os.getenv("SSL_ENABLED", "False") == "True",
+    ssl_certfile=os.getenv("SSL_CERTFILE", None),
+    ssl_keyfile=os.getenv("SSL_KEYFILE", None),
+    port=int(os.getenv("PORT", DEFAULT_PORT)),
+)
 
 
 def get_authorization_url() -> str:
@@ -33,7 +67,7 @@ def get_authorization_url() -> str:
     state = unquote_plus(base64.b64encode(os.urandom(16)).decode("utf-8"))
     app.storage.user["state"] = state
     return (
-        f"{GITHUB_AUTHORIZE_URL}?client_id={CONFIG['CLIENT_ID']}&redirect_uri={CONFIG['REDIRECT_URI']}"
+        f"{GITHUB_AUTHORIZE_URL}?client_id={config.client_id}&redirect_uri={config.redirect_uri}"
         + f"&scope=user&state={state}"
     )
 
@@ -44,10 +78,10 @@ def fetch_access_token(code: str) -> str:
     :param code: The code from GitHub
     :return: The access token"""
     data = {
-        "client_id": CONFIG["CLIENT_ID"],
-        "client_secret": CONFIG["CLIENT_SECRET"],
+        "client_id": config.client_id,
+        "client_secret": config.client_secret,
         "code": code,
-        "redirect_uri": CONFIG["REDIRECT_URI"],
+        "redirect_uri": config.fixed_redirect_uri,
     }
     response = requests.post(GITHUB_ACCESS_TOKEN_URL, data=data)
     response_data = response.text.split("&")
@@ -65,7 +99,7 @@ def fetch_user_data(access_token: str) -> dict:
     return response.json()
 
 
-@ui.page("/login/github/authorized")
+@ui.page(config.redirect_uri_path)
 async def api_gh_callback(request: Request):
     """
     Handles the callback from GitHub
@@ -81,10 +115,10 @@ async def api_gh_callback(request: Request):
         app.storage.user["data"] = user_data
         return RedirectResponse("/")
     else:
-        return RedirectResponse("/login")
+        return RedirectResponse(LOGIN_PATH)
 
 
-@ui.page("/login")
+@ui.page(LOGIN_PATH)
 async def ui_login(client: Client):
     """
     Handles the login page"""
@@ -105,7 +139,7 @@ def ui_index():
     """
     Handles the index page"""
     if not app.storage.user.get("authenticated", False):
-        return RedirectResponse(url="/login")
+        return RedirectResponse(url=LOGIN_PATH)
 
     with ui.header():
         ui.label("NiceGHLogin").classes("font-bold text-2xl")
@@ -117,19 +151,23 @@ def ui_index():
                 on_click=lambda: (
                     app.storage.user.update({"authenticated": False}),
                     app.storage.user.update({"data": {}}),
-                    ui.navigate.to("/login"),
+                    ui.navigate.to(LOGIN_PATH),
                 ),
             )
     ui.label(text=f"Hello {app.storage.user['data']['name']}")
 
 
 if __name__ in {"__main__", "__mp_main__"}:
-    ui.run_with(main, storage_secret=CONFIG["STORAGE_SECRET"])
-    if os.getenv("SSL_ENABLED", "False") == "True":
+    ui.run_with(main, storage_secret=config.storage_secret)
+    if config.enable_ssl:
         import ssl
 
+        if not config.ssl_certfile or not config.ssl_keyfile:
+            raise ValueError("SSL enabled but no certificate or key file provided.")
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-        ssl_context.load_cert_chain(certfile="./cert.pem", keyfile="./key.pem")
-        uvicorn.run(app, host="127.0.0.1", port=5000, ssl_keyfile="./key.pem", ssl_certfile="./cert.pem")
+        ssl_context.load_cert_chain(certfile=config.ssl_certfile, keyfile=config.ssl_keyfile)
+        uvicorn.run(
+            app, host="127.0.0.1", port=config.port, ssl_keyfile=config.ssl_certfile, ssl_certfile=config.ssl_keyfile
+        )
     else:
-        uvicorn.run(app, host="127.0.0.1", port=5000)
+        uvicorn.run(app, host="127.0.0.1", port=config.port)
